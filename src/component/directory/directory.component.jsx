2d6be4aff44fb03b6f83bdbcad6fe4587732f4e9
Siddhesh Kushare <siddheshvikas.kushare2020@vitstudent.ac.in>
2023-08-15 15:06:32 +0530

    update directory

---
+++ src/component/directory/directory.component.jsx
@@ -0,0 +1,11 @@
+import CategoryItem from "../category-item/category-item.component"
+const Directory = ({categories}) => {
+    return (
+        <div className="categories-container">
+     {categories.map((category) => (
+      <CategoryItem category={category} />
+     ))}
+    </div>
+    )
+}
+export default Directory;
